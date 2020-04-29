20bf86dc08f5d87a8826339110ffe1ba75780050
MrRisoni <vanguelisk@gmail.com>
2020-04-29 08:36:58 +0300

    added itinerary

--- src/reducers/air/parkingRdc.js
+++ src/reducers/air/parkingRdc.js
@@ -1,56 +1,8 @@
-import * as MasterCons from '../../actions/master/allConstants';
-
-
-const parkingCost = 0;
-
-
-const parkingDays = 0;
-
-const parkingPricing = [
-  {
-    upToDays: 3,
-    price: 3,
-  },
-  {
-    upToDays: 7,
-    price: 7,
-  },
-  {
-    upToDays: 15,
-    price: 12,
-  },
-];
-
-
-export function getParkingDaysReducer(state = parkingDays, action) {
-    switch (action.type) {
-        case MasterCons.ADD_PARK_DAYS:
-            console.log('adding one hour');
-            if (state < 22) {
-                return ++state;
-            }else {
-                return 0;
-            }
-        case MasterCons.SUB_PARK_DAYS:
-            if (state <=1 ) {
-                return 0;
-            }
-            return --state;
-        default:
-            return state;
-
-    }
-  return parkingDays;
-}
-
-export function getParkPricingReducer(state = parkingPricing, action) {
-  return state;
-}
 
 
 export function getParkPricingFinalCostReducer(state = parkingCost, action) {
 
-  if (action.type == MasterCons.UPSALES_CHANGED) {   
+  if (action.type == MasterCons.UPSALES_CHANGED) {
 
     let totalParkDaysPriced = 0;
 
